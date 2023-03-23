6ad10489e65e584b9dc9b3534fcd83640b3a6d31
SirJohanot <pavelkrafft@gmail.com>
2023-03-23 20:27:02 +0300

    completed the langSyntax tasks

--- src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
+++ src/main/java/com/epam/langSyntax/DigitsSumCalculator.java
@@ -3,6 +3,11 @@ package com.epam.langSyntax;
 public class DigitsSumCalculator {
 
     public void calculateSum(int number) {
+        int digitsSum = 0;
+        for (; number > 0; number /= 10) {
+            digitsSum += number % 10;
+        }
+        System.out.println(digitsSum);
     }
 
 }
--- src/main/java/com/epam/langSyntax/NumberReverter.java
+++ src/main/java/com/epam/langSyntax/NumberReverter.java
@@ -3,6 +3,12 @@ package com.epam.langSyntax;
 public class NumberReverter {
 
     public void revert(int number) {
+        int invertedResult = 0;
+        for (; number > 0; number /= 10) {
+            int rightMostDigit = number % 10;
+            invertedResult = invertedResult * 10 + rightMostDigit;
+        }
+        System.out.println(invertedResult);
     }
 
 }
